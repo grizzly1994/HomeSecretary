0a8fb3f318a686d7bff43e9e0192a36165c9b10f
grizzly1994 <sergejkurakov@gmail.com>
2014-09-03 20:50:43 +0400

    added widgets

--- src/main/java/com/simbircite/demo/controller/RecordsEditController.java
+++ src/main/java/com/simbircite/demo/controller/RecordsEditController.java
@@ -7,31 +7,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/recordsEdit")
 public class RecordsEditController {
-    
+
     private static final String CONTROLLER_PATH = "recordsEdit/";
-    
+
     @RequestMapping(value = "/actions", method = RequestMethod.GET)
     public String actions() {
-	return CONTROLLER_PATH + "actionsEditor";
+        return CONTROLLER_PATH + "actionsEditor";
     }
-    
+
     @RequestMapping(value = "/credits", method = RequestMethod.GET)
     public String credits() {
-	return CONTROLLER_PATH + "creditsEditor";
+        return CONTROLLER_PATH + "creditsEditor";
     }
-    
+
     @RequestMapping(value = "/debts", method = RequestMethod.GET)
     public String debts() {
-	return CONTROLLER_PATH + "debtsEditor";
+        return CONTROLLER_PATH + "debtsEditor";
     }
-    
+
     @RequestMapping(value = "/income", method = RequestMethod.GET)
     public String income() {
-	return CONTROLLER_PATH + "incomeEditor";
+        return CONTROLLER_PATH + "incomeEditor";
     }
-    
+
     @RequestMapping(value = "/accumulations", method = RequestMethod.GET)
     public String accumulations() {
-	return CONTROLLER_PATH + "accumulationsEditor";
+        return CONTROLLER_PATH + "accumulationsEditor";
     }
 }
