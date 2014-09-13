fbef3fa60f853b2163089efb589001855576f990
grizzly1994 <sergejkurakov@gmail.com>
2014-09-13 18:30:23 +0400

    added data binding

--- src/main/java/com/simbircite/demo/controller/EditController.java
+++ src/main/java/com/simbircite/demo/controller/EditController.java
@@ -1,21 +1,27 @@
 package com.simbircite.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import com.simbircite.homesecretary.entity.*;
 
 @Controller
 @RequestMapping("/edit")
 public class EditController {
     
-    @RequestMapping(value = "{report}", method = RequestMethod.GET)
-    public String show(@PathVariable("report") String report) {
-        return "edit/" +  report;
+    @RequestMapping(value = "actions", method = RequestMethod.GET)
+    public ModelAndView show() {
+    	Transaction transaction = new Transaction();
+    	transaction.setComment("azaza");
+    	return new ModelAndView("edit/actions", "transaction", transaction);
     }
     
-    @RequestMapping(value = "{report}", method = RequestMethod.POST)
-    public String edit(@PathVariable("report") String report) {
+    @RequestMapping(value = "actions", method = RequestMethod.POST)
+    public String edit(@ModelAttribute("transaction") Transaction transaction) {
+    	System.out.println(transaction.getComment());
         return "redirect:/reports";
     }
 }
